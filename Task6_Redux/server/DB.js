9aaafa7a51a1a7b8dab553351cb54a3efcc7e680
KindCoffeBear <conter123@list.ru>
2022-04-05 22:27:22 +0300

    Redux

---
+++ Task6_Redux/server/DB.js
@@ -0,0 +1,29 @@
+const db = {
+  posts: [
+    {
+      idPost: 1,
+      head: 'Животный мир',
+      description: 'Белка очень красивая',
+      link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR5rcA2JB9A8BA8QRXXhVP1PCFoWPL3IFHy8bAlQZMfwHpjlTooLrvEn9Vlbx3xfNKXHw&usqp=CAU',
+      tag: 'Белка',
+    },
+    {
+      idPost: 2,
+      head: 'Морской мир',
+      description: 'Я люблю морских коньков',
+      link: 'https://i2.wp.com/fb.ru/misc/i/gallery/60329/2612181.jpg',
+      tag: 'Конек',
+    },
+    {
+      idPost: 3,
+      head: 'Автомир',
+      description: 'Железный конь',
+      link: 'https://cdn.motor1.com/images/mgl/7xNMo/s1/bulletproof-ferrari-458-speciale-by-addarmor-front-view.jpg',
+      tag: 'Феррари',
+    },
+  ],
+}
+
+module.exports = {
+  db,
+}
